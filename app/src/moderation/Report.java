8f30104bb53c7a28648096c3492403ff70196dda
Youwei WAN <u8109579@anu.edu.au>
2026-05-08 14:30:05 +1000

    report modified

---
+++ app/src/moderation/Report.java
@@ -0,0 +1,12 @@
+package moderation;
+
+import java.util.UUID;
+
+/**
+ * 一条活跃举报记录，表示某个用户在某个时间举报了某条消息。
+ *
+ * @param message 被举报消息的 UUID
+ * @param user 发起举报的用户 UUID
+ * @param timestamp 举报发生的时间戳
+ */
+record Report(UUID message, UUID user, long timestamp) {}
